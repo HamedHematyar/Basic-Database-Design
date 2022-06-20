9bcc3860a90999cc77cb9d3f4186a0ad1fc130c1
securerender <securerender@gmail.com>
2022-06-20 09:11:42 -0700

    [DEV] init

--- test.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name='', phone='', address=''):
-        self.address = address
-        self.phone = phone
-        self.name = name
-
-
-person = Person('hamed', '333', 'Wilson Ave')
-print(person)
